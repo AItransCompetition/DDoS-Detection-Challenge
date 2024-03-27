7b2861c9b7ee369855ef1200c0eb3e64144372bc
ckw20 <78785236+ckw20@users.noreply.github.com>
2024-03-27 09:36:51 +0800

    update

--- checker.cpp
+++ checker.cpp
@@ -1,100 +1,51 @@
 #include "testlib.h"
-#include <string.h>
+#include <string>
 #include <assert.h>
-#include <vector>
-#include <utility>
 #include <set>
 
-inline int toInt(std::string s, int maxv) {
-	int len = (int)s.length(), val = 0;
-	for (int i = 0; i < len; i++) {
-		quitif(s[i] < '0' || s[i] > '9', _wa, "Invalid IPv4 address.");
-		val = val * 10 + s[i] - '0';
-		quitif(val > maxv, _wa, "Invalid IPv4 address.");
-	}
-	return val;
-}
-
-inline std::string toIPv4(long long ip) {
-	return std::to_string((ip >> 24) & 255) + "." + std::to_string((ip >> 16) & 255) + "." + std::to_string((ip >> 8) & 255) + "." + std::to_string(ip & 255);
-}
-
-int ps[10];
-inline std::vector<std::pair<long long, int> > readIPv4AndLabels(InStream &f, int IP_cnt) {
-	std::vector<std::pair<long long, int> > result;
-	for (int i = 1; i <= IP_cnt; i++) {
-		long long ip = 0;
-		int label = -1;
-		
-		std::string s = f.readString();
-		int cur = 0, cnt = 0, len = (int)s.length();
-
-		quitif(s[len - 1] != '0' && s[len - 1] != '1' && s[len - 2] != ' ', _wa, "Label (0 or 1) expected.");
-		label = s[len - 1] - '0';
-
-		for (int i = 0; i < len - 2; i++) {
-			if (s[i] == '.') ps[cnt++] = i;
-			quitif(cnt > 3, _wa, "Invalid IPv4 address.");
-		}
-		quitif(cnt != 3, _wa, "Invalid IPv4 address.");
-		ps[3] = len - 2;
-		ip = toInt(s.substr(0, ps[0]), 255);
-		ip = ip * 256 + toInt(s.substr(ps[0] + 1, ps[1] - ps[0] - 1), 255);
-		ip = ip * 256 + toInt(s.substr(ps[1] + 1, ps[2] - ps[1] - 1), 255);
-		ip = ip * 256 + toInt(s.substr(ps[2] + 1, ps[3] - ps[2] - 1), 255);
-
-		result.push_back(std::make_pair(ip, label));
-	}
-	return result;
-}
-
-
-std::set<long long> attack_ips, normal_ips, ips, ouf_ips;
+std::set<std::string> benign, predict;
 
 int main(int argc, char **argv) {
-	registerLocalChecker(std::string(argv[1]), std::string(argv[2]), std::string(argv[3]), 100, std::string(argv[4]));
+	registerLocalChecker(std::string(""), std::string(argv[1]), std::string(argv[2]), 100, std::string(argv[3]));
 
-	int IP_cnt = ans.readInt(0, 1000000000);
-	quitif(ouf.readInt(0, 1000000000) != IP_cnt, _wa, "Number of IP should be %d.\n", IP_cnt);
-	ans.readEoln(), ouf.readEoln();
+	int flow_cnt = ans.readInt();
+	if (ouf.readInt() != flow_cnt) quitp(0, "Wrong flow number.\n");
+	ans.eoln(), ouf.eoln();
 
-	int positive = 0, negtive = 0, correct = 0, true_positive = 0, false_positive = 0, false_negtive = 0;
-
-	std::vector<std::pair<long long, int> > ip_list = readIPv4AndLabels(ans, IP_cnt);
-	for (auto ip_label : ip_list) {
-		long long ip = ip_label.first;
-		int label = ip_label.second;
-
-		assert(!ips.count(ip)), ips.insert(ip);
-		if (label) attack_ips.insert(ip), positive += 1;
-		else normal_ips.insert(ip), negtive += 1;
+	while (true) {
+		benign.insert(ans.readString());
+		ans.eoln();
+		if (ans.eof()) break;
 	}
 
-	ip_list = readIPv4AndLabels(ouf, IP_cnt);
-	for (auto ip_label : ip_list) {
-		long long ip = ip_label.first;
-		int label_predict = ip_label.second;
+	int true_positive = 0;
+	while (true) {
+		std::string flow_id = ouf.readString();
+		if (predict.count(flow_id)) quitp(0, "Duplicated flow id.\n");
+		predict.insert(flow_id);
+		if ((int)predict.size() > flow_cnt) quitp(0, "Too many flows.\n");
+		if (benign.count(flow_id)) true_positive += 1;
 
-		quitif(!ips.count(ip), _wa, "No source IP: %s.\n", toIPv4(ip).c_str());
-		quitif(ouf_ips.count(ip), _wa, "Duplicated source IP: %s.\n", toIPv4(ip).c_str());
-		ouf_ips.insert(ip);
-
-		int label = attack_ips.count(ip);
-		if (label_predict == 1 && label == 1) true_positive += 1, correct += 1;
-		else if (label_predict == 1 && label == 0) false_positive += 1;
-		else if (label_predict == 0 && label == 1) false_negtive += 1;
-		else correct += 1;
+		ouf.eoln();
+		if (ouf.eof()) break;
 	}
 
-	ans.readEof(), ouf.readEof();
-	double accuracy = (double) correct / IP_cnt;
-	double precision = (double) true_positive / (true_positive + false_positive);
-	double recall = (double) true_positive / (true_positive + false_negtive);
-	double F1_score = 2.0 * precision * recall / (precision + recall);
+	int false_negtive = (int)benign.size() - true_positive;
+	int false_positive = (int)predict.size() - true_positive;
+	if (true_positive + false_positive + false_negtive > flow_cnt)
+		quitp(0, "Too many flows.\n");
+	int true_negtive = flow_cnt - (true_positive + false_positive + false_negtive);
+	int correct = true_positive + true_negtive;
 
-	quitp(F1_score * 100,
+	if (true_positive == 0) quitp(0, "No True Positive...\n");
+	long double accuracy = (long double) correct / flow_cnt;
+	long double precision = (long double) true_positive / (true_positive + false_positive);
+	long double recall = (long double) true_positive / (true_positive + false_negtive);
+	long double F1_score = (long double) 2.0 * precision * recall / (precision + recall);
+
+	quitp((double) F1_score * 100,
 		"accuracy = %.2lf%%, precision = %.2lf%%, recall = %.2lf%%, F1_score = %.4lf.\n",
-		accuracy * 100, precision * 100, recall * 100, F1_score);
+		(double) accuracy * 100, (double) precision * 100, (double) recall * 100, (double) F1_score);
 	
 	return 0;
 }
--- dataset/Stage0/README.md
+++ dataset/Stage0/README.md
@@ -1,3 +1,11 @@
-#### Download
+#### Download & Unzip
 
-Please download the dataset from [https://cloud.tsinghua.edu.cn/d/4b637dc88c934085bb38/](https://cloud.tsinghua.edu.cn/d/4b637dc88c934085bb38/) or [https://www.unb.ca/cic/datasets/ddos-2019.html](https://www.unb.ca/cic/datasets/ddos-2019.html).
+Please download & unzip the dataset from [https://www.unb.ca/cic/datasets/ddos-2019.html](https://www.unb.ca/cic/datasets/ddos-2019.html).
+
+This task expects the contestant to consider the dataset generated on the first day as a training dataset and the dataset generated on the second day as a test dataset, train with the labels from the training dataset as well as the issued training set, and generate prediction labels in the same format on the test dataset and submit them to the website for testing.
+
+#### Format of train_labels.txt
+
+The first line indicates the number of flows. Each flow is represented by a quintuple: {Source IP}-{Destination IP}-{Source Port}-{Destination Port}-{Protocol}.
+
+The submitted file's format should be the same.
